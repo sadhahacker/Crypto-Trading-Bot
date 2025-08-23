8bb91aa128f74349ec6633c99d8dc5485f59a141
sadhasivam <sadha30102001@gmail.com>
2025-08-23 20:56:04 +0530

    add snadcn

---
+++ resources/js/app.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Dashboard from "@/pages/Dashboard"
+
+const root = ReactDOM.createRoot(document.getElementById("app")!)
+root.render(<Dashboard />)
